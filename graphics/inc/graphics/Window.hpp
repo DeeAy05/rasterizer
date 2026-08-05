af721e9d25e80a37c51139dfbce997df5a7f31bb
DeeAy05 <davidakopan005@gmail.com>
2026-08-06 00:31:03 +0200

    Adding window class and refactoring 01-ClearScreen Sample

---
+++ graphics/inc/graphics/Window.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <SDL3/SDL_events.h>
+#include <string_view>
+
+struct SDL_Window;
+struct SDL_Renderer;
+
+namespace rasterizer
+{
+inline namespace graphics
+{
+class Window
+{
+public:
+    Window() = default;
+    ~Window();
+
+    Window( std::string_view title, int width, int height, bool fullscreen = false );
+    Window( const Window& ) = delete;
+    Window( Window&& window ) noexcept;
+
+    Window& operator=( const Window& ) = delete;
+    Window& operator=( Window&& ) noexcept;
+
+    explicit operator bool() const;
+
+    void create( std::string_view title, int width, int height, bool fullscreen = false );
+
+    void destroy();
+
+    void close();
+
+    bool isValid() const;
+
+    void clear( uint8_t r, uint8_t b, uint8_t g, uint8_t a = 255 );
+
+    void present();
+
+    void resize( int width, int height );
+
+    void setFullscreen( bool fullscreen );
+
+    void toggleFullscreen();
+
+    bool isFullscreen() const noexcept;
+
+    void setVSync( bool vSync );
+
+    void toggleVSync();
+
+    bool isVSync() const noexcept;
+
+
+private:
+    static bool SDLCALL eventWatch( void* userdata, SDL_Event* event );
+
+    SDL_Window*   m_Window;
+    SDL_Renderer* m_Renderer;
+    int           m_Width      = -1;
+    int           m_Height     = -1;
+    bool          m_Fullscreen = false;
+    bool          m_VSync      = true;
+    bool          m_Close      = false;
+
+};  // class Window
+}  // namespace graphics
+}  // namespace rasterizer
--- samples/01-ClearScreen/main.cpp
+++ samples/01-ClearScreen/main.cpp
@@ -1,60 +1,46 @@
-#include <SDL3/SDL.h>
+#include <graphics/Window.hpp>
 
-constexpr int SCREEN_WIDTH = 800;
-constexpr int SCREEN_HEIGTH = 600;
-constexpr const char* TITLE = "01 - ClearScreen";
+constexpr int         SCREEN_WIDTH  = 800;
+constexpr int         SCREEN_HEIGHT = 600;
+constexpr const char* TITLE         = "01 - ClearScreen";
+
+using namespace rasterizer::graphics;
 
 int main()
 {
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to initialize SDL: %s", SDL_GetError());
-        return -1;
-    }
-
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-    SDL_WindowFlags flags = SDL_WINDOW_RESIZABLE;
-    if (!SDL_CreateWindowAndRenderer(TITLE, SCREEN_WIDTH, SCREEN_HEIGTH, flags, &window, &renderer))
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to create window and renderer: %s", SDL_GetError());
-        return -2;
-    }
-    bool vSync = true;
-    SDL_SetRenderVSync(renderer, vSync ? 1 : 0);
-    bool running = true;
-    while (running)
+    Window window( TITLE, SCREEN_WIDTH, SCREEN_HEIGHT );
+    while ( window )
     {
         SDL_Event e;
-        while (SDL_PollEvent(&e))
+        while ( SDL_PollEvent( &e ) )
         {
-            switch (e.type)
+            switch ( e.type )
             {
             case SDL_EVENT_QUIT:
-                running = false;
+                window.close();
                 break;
             case SDL_EVENT_KEY_DOWN:
-                switch (e.key.key) 
+                switch ( e.key.key )
                 {
                 case SDLK_ESCAPE:
-                    running = false;
+                    window.close();
                     break;
                 case SDLK_V:
-                    vSync = !vSync;
-                    SDL_SetRenderVSync(renderer, vSync ? 1 : 0);
+                    window.toggleVSync();
                     break;
+                case SDLK_RETURN:
+                    if ( ( e.key.mod & SDL_KMOD_ALT ) != 0 )
+                    {
+                        case SDLK_F11:
+                        window.toggleFullscreen();
+                    }
+                        break;
                 }
                 break;
             }
         }
-
-        SDL_SetRenderDrawColor(renderer, 154, 206, 235, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+        window.clear( 154, 206, 235 );
+        window.present();
     }
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
     return 0;
 }
